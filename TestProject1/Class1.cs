5f8e723d694f86b63a22a6a388a9fee96606ff0e
JaniTuoriniemi <janituoriniemi@hotmail.com>
2023-10-10 15:33:02 +0200

    ee

---
+++ TestProject1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Parameters
+{
+	public Main()
+	{public class Testparameters()
+	{ string ExistingAccountPhone;
+      string CreatedAccountPhone;
+	  string ExistingAccountPassword;
+		string bookQRcode;
+		string boookID;
+
+		class  Browserprameters()
+		{ string Browsertype
+
+				Class Headless(string parameter)
+				string Parameter
+				Headless(parameter)
+			{ Parameter=parameter; }
+            Class ShouldClose(string parameter)
+
+                string Parameter
+
+                ShouldClose(parameter)
+            { Parameter = parameter; }
+			{ }
+
+
+		}
+
+
+
+
+
+	}
+	}
+}
